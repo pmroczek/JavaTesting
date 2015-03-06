850517b2bbf63b86384413ba778de8eb965b3e17
pmroczek <pmroczek@outlook.com>
2015-03-06 15:26:31 +0100

    Add suite to lab1/zad3

---
+++ lab1/zad3/AllTests.java
@@ -0,0 +1,10 @@
+package zad3;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ RomanNumbersParametrizedTests.class, RomanNumberTests.class })
+public class AllTests {
+}
